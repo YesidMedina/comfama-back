6591d972c4d250e65909daa5faffb4bbd04aa82c
YesidMedina <84429389+YesidMedina@users.noreply.github.com>
2022-08-25 20:31:57 -0500

    comfama back

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { app } = require("./lib/infrastructure/webserver/server");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
